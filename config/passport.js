aa52d531c5eeef238de43cf7381edfc5f04ac3f3
Vico509 <antoinejovany99@gmail.com>
2024-03-28 12:43:49 -0400

    Configuring the verify callback function

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,35 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const connection = require('./database');
+const { validPassword } = require('../lib/passwordUtils');
 const User = connection.models.User;
 
-// TODO: passport.use();
+const customFields = {
+    usernameField: 'uname',
+    passwordField: 'pw'
+};
+
+const verifyCallback =  (username, password, done) => {
+
+    User.findOne({ username: username})
+            .then((user) => {
+
+                if (!user) { return done(null, false)}
+
+                // Function defined at bottom of app.js
+                const isValid = validPassword(password, user.hash, user.salt);
+
+                if (isValid) {
+                    return done(null, user);
+                } else {
+                    return done(null, false);
+                }
+            })
+            .catch((err) => {
+                done(err);
+            });
+}
+
+const strategy = new LocalStrategy();
+
+
