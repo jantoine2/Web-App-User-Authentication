ac336fb19520f3e784273ba69a7859ffd31b8d59
Vico509 <antoinejovany99@gmail.com>
2024-04-02 01:25:15 -0400

    protecting data decrypt

---
+++ cryptography/decrypt.js
@@ -0,0 +1,8 @@
+const crypto = require('crypto');
+
+function decryptWithPrivateKey(privateKey, encryptedMessage) {
+
+    return crypto.privateDecrypt(privateKey, encryptedMessage);
+}
+
+module.exports.decryptWithPrivateKey = decryptWithPrivateKey;
--- cryptography/main.js
+++ cryptography/main.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const encrypt = require('./encrypt');
+const decrypt = require('./decrypt');
 
 const publicKey = fs.readFileSync(__dirname + '/id_rsa_pub.pem', 'utf-8');
 
@@ -8,3 +9,9 @@ const encryptedMessage = encrypt.encryptWithPublicKey(publicKey, 'Super secret m
 
 // I you try and 'crack the code', you will just get gibberish
 console.log(encryptedMessage.toString());
+
+const privateKey = fs.readFileSync(__dirname + '/id_rsa_priv.pem', 'utf-8');
+
+const decryptedMessage = decrypt.decryptWithPrivateKey(privateKey, encryptedMessage);
+
+console.log(decryptedMessage.toString());
